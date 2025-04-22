22b66f8e3e26378229b9ff886106fd2a59b92c0c
MilkyWay14 <chesco817@gmail.com>
2025-04-21 22:41:19 -0500

    Update final arbol

--- arbol.py
+++ arbol.py
@@ -1,139 +1,21 @@
-# ===========================================
-# By: Juan Niño 2240040- Franchesco Hernandez 2240032- Estructuras Datos
-# Name: Arboles con Listas Doblemente Enlazadas 
-# ===========================================
+# ========================================================================
+# By: Juan Niño 2240040 - Franchesco Hernandez 2240032 - Estructuras Datos
+# Name: Arboles con Listas simplemente enlazadas
+# ========================================================================
 
 # Clase Nodo
+
+
 class Nodo:
 	def __init__(self, data):
 		self.data = data
-		self.siguiente = None
-
-# CLase Listas enlazada simple
-class Hijo:
-	def __init__(self):
-		self.cabeza = None
-  
-  	# Lista Vacia
-	def vacio(self):
-		if self.cabeza == None:
-			print("Está vacia")
-		else:
-			print("Lista no vacia")
-
-	# Agregar al inicio
-	def agregarInicio(self, data):
-		nuevo_nodo = Nodo(data)
-		if self.cabeza is None:
-			self.cabeza = nuevo_nodo
-			return
-		else:
-			nuevo_nodo.siguiente = self.cabeza
-			self.cabeza = nuevo_nodo
-
-	# Imprimir lista
-	def imprimirLista(self):
-		print('[', end="")
-		if self.cabeza == None:
-			print(' ]')
-			return
-		temp = self.cabeza
-		while( True ):
-			print("",temp.data, end="")
-			if (temp.siguiente == None):
-				break
-			temp = temp.siguiente
-			print(" ,", end="")
-		print(' ]')
-  
-	# Buscar último
-	def ultimo(self):
-		temp = self.cabeza
-		while (True):
-			if temp.siguiente == None:
-				return temp
-			temp = temp.siguiente
-   
-	# Insertar al final
-	def agregarFinal(self,data):
-		nuevo_nodo = Nodo(data)
-		if self.cabeza is None:
-			self.cabeza = nuevo_nodo
-			return
-		temp = self.ultimo()
-		temp.siguiente = nuevo_nodo
-
-	# Eliminar primero
-	def eliminarInicio(self):
-		self.cabeza = self.cabeza.siguiente
-
-	# Eliminar último
-	def eliminarUltimo(self):
-		if (self.cabeza.siguiente == None):
-			self.cabeza = None
-			return
-		temp = self.cabeza
-		while( True ):
-			if temp.siguiente.siguiente == None:
-				temp.siguiente = None
-				return
-			temp = temp.siguiente
-
-	# Buscar un elemento por su valor
-	def buscar(self,data):
-		temp = self.cabeza
-		while ( True ):
-			if temp.data == data:
-				return True
-			if temp.siguiente == None:
-				return False
-			temp = temp.siguiente
-
-    # Insertar valor data antes del valor x
-	def insertarAntes(self,data,x):
-		if not self.buscar(x) :
-			print('El valor', data, 'no está en la lista')
-			return
-		if self.cabeza.data == x:
-			self.agregarInicio(data)
-			return
-		nuevo_nodo = Nodo(data)
-		temp = self.cabeza
-		while ( True ):
-			if temp.siguiente.data == x:
-				nuevo_nodo.siguiente = temp.siguiente
-				temp.siguiente = nuevo_nodo
-				return
-			temp = temp.siguiente
-
-	# Insertar valor data despues del valor x
-	def insertarDespues(self,data,x):
-		if not self.buscar(x) :
-			print('El valor', data, 'no está en la lista')
-			return
-		nuevo_nodo = Nodo(data)
-		temp = self.cabeza
-		while ( True ):
-			if temp.data == x:
-				nuevo_nodo.siguiente = temp.siguiente
-				temp.siguiente = nuevo_nodo
-				return
-			temp = temp.siguiente
-
-	# Contar la cantidad de elementos
-	def contar(self):
-		temp = self.cabeza
-		cont = 1
-		while ( True ):
-			if temp.siguiente == None:
-				return cont
-			cont += 1
-			temp = temp.siguiente
-		
-  
-  
+		self.hijo = None
+		self.hermano = None
+	
+	def toString(self):
+		return self.data.string()
   	
-# CLase Listas enlazada simple
+# CLase Lista enlazada simple modificada
 class Arbol:
 	def __init__(self):
 		self.raiz = None
@@ -144,94 +26,123 @@ class Arbol:
 			print("Está vacia")
 		else:
 			print("Lista no vacia")
-
-	# Agregar la raiz del arbol
-	def agregarRaiz(self, data):
-		nuevo_nodo = Nodo(data)
-		self.raiz=nuevo_nodo
-
  
- 	# Insertar al final
+ 	# Insertar hijos a la raiz
 	def agregarHijo(self, data):
+		if self.raiz is None:
+			nuevo_nodo = Nodo(data)
+			self.raiz = nuevo_nodo
+			return
+		self.agregarHijoNodo(self.raiz,data)
+
+	def agregarHijoNodo(self, nodo, data):
 		nuevo_nodo = Nodo(data)
-		if self.cabeza is None:
-			self.cabeza = nuevo_nodo
+		if nodo.hijo is None:
+			nodo.hijo = nuevo_nodo
 			return
-		temp = self.ultimo()
-		temp.siguiente = nuevo_nodo
-  
-	# Imprimir lista
-	def toStringPreOrden(self):
-		
-	def toStringPosOrden(self):
-	def toStringInOrden(self):
-	def toStringPorNiveles(self):
-   
-
-
-	# Eliminar primero
-	def eliminarInicio(self):
-		self.cabeza = self.cabeza.siguiente
-
-	# Eliminar último
-	def eliminarUltimo(self):
-		if (self.cabeza.siguiente == None):
-			self.cabeza = None
-			return
-		temp = self.cabeza
-		while( True ):
-			if temp.siguiente.siguiente == None:
-				temp.siguiente = None
+		temp = nodo.hijo
+		while(True):
+			if temp.hermano is None:
+				temp.hermano = nuevo_nodo
 				return
-			temp = temp.siguiente
+			temp = temp.hermano
 
-	# Buscar un elemento por su valor
-	def buscar(self,data):
-		temp = self.cabeza
-		while ( True ):
-			if temp.data == data:
-				return True
-			if temp.siguiente == None:
-				return False
-			temp = temp.siguiente
+	def getPeso(self):
+		if self.raiz is None:
+			return 0
+		return self.getPesoNodo(self.raiz)
 
-    # Insertar valor data antes del valor x
-	def insertarAntes(self,data,x):
-		if not self.buscar(x) :
-			print('El valor', data, 'no está en la lista')
-			return
-		if self.cabeza.data == x:
-			self.agregarInicio(data)
-			return
-		nuevo_nodo = Nodo(data)
-		temp = self.cabeza
-		while ( True ):
-			if temp.siguiente.data == x:
-				nuevo_nodo.siguiente = temp.siguiente
-				temp.siguiente = nuevo_nodo
-				return
-			temp = temp.siguiente
+	def getPesoNodo(self,nodo):
+		if nodo.hijo is None:
+			return 1
+		peso = 1
+		temp = nodo.hijo
+		while(True):
+			peso += self.getPesoNodo(temp)
+			if temp.hermano is None:
+				return peso
+			temp = temp.hermano
 
-	# Insertar valor data despues del valor x
-	def insertarDespues(self,data,x):
-		if not self.buscar(x) :
-			print('El valor', data, 'no está en la lista')
-			return
-		nuevo_nodo = Nodo(data)
-		temp = self.cabeza
-		while ( True ):
-			if temp.data == x:
-				nuevo_nodo.siguiente = temp.siguiente
-				temp.siguiente = nuevo_nodo
-				return
-			temp = temp.siguiente
+	def getOrden(self):
+		if self.raiz is None:
+			return 0
+		return self.getOrdenNodo(self.raiz)
 
-	# Contar la cantidad de elementos
-	def contar(self):
-		temp = self.cabeza
-		cont = 1
-		while ( True ):
-			if temp.siguiente == None:
-				return cont
+	def getOrdenNodo(self,nodo):
+		if nodo.hijo is None:
+			return 0
+		temp = nodo.hijo
+		cont = 0
+		while True:
 			cont += 1
-			temp = temp.siguiente
+			if temp.hermano is None:
+				break
+			temp = temp.hermano
+		temp = nodo.hijo
+		while True:
+			orden = self.getOrdenNodo(temp)
+			if orden > cont:
+				cont = orden
+			if temp.hermano is None:
+				return cont
+			temp = temp.hermano
+
+	def getAltura(self):
+		if self.raiz is None:
+			return 0
+		return self.getAlturaNodo(self.raiz)
+
+	def getAlturaNodo(self,nodo):
+		if nodo.hijo is None:
+			return 1
+		temp = nodo.hijo
+		if temp.hermano == None:
+			return self.getAlturaNodo(temp)+1
+		t = self.getAlturaNodo(temp)
+		while(True):
+			h = self.getAlturaNodo(temp.hermano)
+			if h > t:
+				t = h 
+			if temp.hermano.hermano is None:
+				return t+1
+			temp = temp.hermano
+
+arbol = Arbol()
+arbol.agregarHijo(input('Ingrese el dato del nodo raíz: '))
+select=arbol.raiz
+while(True):
+	selec = input(f'\nNodo seleccionado: {select.data}\n1. Agregar hijo al nodo seleccionado.\n2. Seleccionar un hijo del nodo seleccionado.\n3. Volver a la raiz.\n4. Peso, altura y orden del árbol.\n5. Salir.\n\nElija una opción: ')
+	if selec == "1":
+		arbol.agregarHijoNodo(select,input("Ingrese el dato del nodo hijo: "))
+	elif selec == "2":
+		print(f"\nNodos hijos del nodo {select.data}:\n")
+		if select.hijo is None:
+			print("No hay hijos")
+		else:
+			temp = select.hijo
+			cont=0
+			while True:
+				cont+=1
+				print(f"{cont}. {temp.data}")
+				if temp.hermano is None:
+					break
+				temp=temp.hermano
+			elec = int(input("\nEscriba el número del nodo a elegir: "))
+			temp = select.hijo
+			cont=0
+			while True:
+				cont+=1
+				if cont == elec:
+					select = temp
+				if temp.hermano is None:
+					break
+				temp=temp.hermano
+	elif selec == "3":
+		select = arbol.raiz
+	elif selec == "4":
+		print("\nPeso del arbol: ", arbol.getPeso())
+		print("Orden del arbol: ", arbol.getOrden())
+		print("Altura del arbol: ", arbol.getAltura())
+	elif selec == "5":
+		print("\n\nSaliendo del programa...\n")
+		break
